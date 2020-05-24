76b17f371b7f1a9bc3f26919e1dcd72a1de1bd59
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-05-24 13:35:35 +0100

    Add mock data files for LS journey slice inc. multi-select services (#8)

--- src/main/java/uk/gov/crowncommercial/dts/scale/service/gm/service/DecisionTreeService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/service/gm/service/DecisionTreeService.java
@@ -1,6 +1,8 @@
 package uk.gov.crowncommercial.dts.scale.service.gm.service;
 
 import java.util.Set;
+import java.util.StringJoiner;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import uk.gov.crowncommercial.dts.scale.service.gm.model.AnsweredQuestion;
@@ -29,10 +31,14 @@ public class DecisionTreeService {
   public GetJourneyQuestionOutcomeResponse getJourneyQuestionOutcome(final String journeyInstanceId,
       final String questionId, final Set<AnsweredQuestion> answeredQuestions) {
 
+    // First 8 digits of the answer UUIDs is sufficient
+    final StringJoiner sj = new StringJoiner(",");
+    answeredQuestions.stream().findFirst().get().getAnswers().stream()
+        .map(a -> StringUtils.substringBefore(a, "-")).sorted().forEach(sj::add);
+
     // TODO: Get history from JourneyInstance repo?
-    String fileName =
-        String.format("get-journey-question-outcome/%s_%s_%s.json", journeyInstanceId, questionId,
-            answeredQuestions.stream().findFirst().get().getAnswers().stream().findFirst().get());
+    String fileName = String.format("get-journey-question-outcome/%s_%s_(%s).json",
+        journeyInstanceId, questionId, sj.toString());
     return dataLoader.convertJsonToObject(fileName, GetJourneyQuestionOutcomeResponse.class);
 
   }
